59741dafa690df8c8a5234774516311a3cb8ce03
ShevchukA <antoshka.vbg@mail.ru>
2025-02-20 20:03:53 +0000

    fix: delete unnecessary font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -13,7 +13,6 @@ export default function RootLayout() {
     'Afacad-Medium': require('../assets/fonts/Afacad-Medium.ttf'),
     'Afacad-Regular': require('../assets/fonts/Afacad-Regular.ttf'),
     'Afacad-SemiBold': require('../assets/fonts/Afacad-SemiBold.ttf'),
-    'AlbertSans-SemiBold': require('../assets/fonts/AlbertSans-SemiBold.ttf'),
     'Jost-Regular': require('../assets/fonts/Jost-Regular.ttf'),
     'Jost-SemiBold': require('../assets/fonts/Jost-SemiBold.ttf'),
   });
