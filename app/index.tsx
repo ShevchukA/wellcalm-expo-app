90067acb043f25c2f7828ec33b40d8aa104a9211
ShevchukA <antoshka.vbg@mail.ru>
2025-02-28 23:56:52 +0000

    feat: update icon images

--- app/index.tsx
+++ app/index.tsx
@@ -12,8 +12,11 @@ export default function Index() {
   return (
     <View style={styles.screenLayout}>
       <SafeAreaView style={styles.screenLayout}>
-        <Text style={styles.logo}>WELLCALM</Text>
-        <Text style={styles.title}>to the tracking mode</Text>
+        <View style={styles.logoContainer}>
+          <Text style={styles.logo}>WELLCALM</Text>
+          <Text style={styles.title}>to the tracking mode</Text>
+        </View>
+
         <View style={styles.buttonContainer}>
           <Button text='Start' appearance='primary' onPress={handleStart} />
         </View>
@@ -29,16 +32,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: Colors.mainBlue,
   },
+  logoContainer: {
+    width: 341,
+    height: 341,
+    borderWidth: 1,
+  },
   logo: {
     fontFamily: 'Afacad-Semibold',
     fontSize: 48,
     lineHeight: 48,
+    textAlign: 'center',
     color: Colors.mainWhite,
   },
   title: {
     fontFamily: 'Afacad-Regular',
     fontSize: 24,
     lineHeight: 24,
+    textAlign: 'center',
     color: Colors.mainWhite,
     marginTop: -8,
   },
